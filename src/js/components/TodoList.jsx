575eefb195706a6b6be34a615b936fe9512a954b
Jorgee-hub <jmontiek217@gmail.com>
2026-06-20 04:49:52 +0000

    todo listo, pruebas realizadas y completadas con exito

---
+++ src/js/components/TodoList.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const TodoList = () => {
+    const [task, setTask] = useState("");
+    const [list, setList] = useState([]);
+    const keyboardControl = (event) => {
+        if (event.key === "Enter") {
+            setList([...list, task])
+            setTask("");
+        }
+    }
+    const deleteTask = (indexToDelete) => {
+        const newList = list.filter((_, index) => index !== indexToDelete);
+        setList(newList);
+    }
+    return (
+        <>
+            <div className="d-flex justify-content-center">
+                <h1 className="text-dark">Todos</h1>
+            </div>
+            <div className="todo-box">{/* Contenedor principal */}
+                <input value={task} onKeyDown={keyboardControl} type="text" placeholder="Agregar nueva tarea" onChange={(e) => { setTask(e.target.value) }} />
+                <ul>{/* lista de tareas */}
+                    {list.map((item, index) => (
+                        <li key={index}>
+                            {item}
+                            <span className="deleteBtn text-secondary" onClick={() => deleteTask(index)}>x</span>
+                        </li>
+                    ))}
+                </ul>
+                <footer className="m-1 text-secondary fs-6">
+                    {list.length} {list.length === 1 ? "item left" : "items left"}
+                </footer>
+            </div>
+        </>
+    )
+}
+export default TodoList;
